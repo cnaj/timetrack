f90a250596f94ac2999d7d6bb4353ccff724d9e9
Tomas Cernaj <tomas@cernaj.de>
2019-11-19 07:39:36 +0100

    Output entries

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 #![allow(dead_code)]
 
-use crate::fileread::read_log_lines;
+use crate::fileread::{read_log_lines, LogLine};
 use std::env;
 
 pub mod fileread;
@@ -10,11 +10,11 @@ fn main() -> Result<(), String> {
     let args: Vec<String> = env::args().collect();
 
     if args.len() != 2 {
-        println!("Usage: timetrack LOG_FILE_PATH");
+        println!("Usage: {} LOG_FILE_PATH", args[0]);
         std::process::exit(1);
     }
 
-    print_lines(args[1].as_str())?;
+    gather_tasks(args[1].as_str())?;
     Ok(())
 }
 
@@ -35,7 +35,23 @@ fn gather_tasks(path: &str) -> Result<(), String> {
     let lines = read_log_lines(path)
         .map_err(|err| format!("Could not read file: {}", err))?;
 
-    // TODO
+    let lines = lines
+        .filter_map(|res| match res {
+            Ok(line) => {
+                match line {
+                    LogLine::Entry(entry) => Some(Ok(entry)),
+                    LogLine::Ignored => None,
+                }
+            },
+            Err(err) => Some(Err(err)),
+        });
+
+    for line in lines {
+        match line {
+            Ok(entry) => println!("{:?}", entry),
+            Err(err) => println!("Error: {}", err),
+        }
+    }
 
     Ok(())
 }
